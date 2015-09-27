f37bd73b3b9361e3d6f1a0080baf1017c9b92e5a
Carlos Reig Matut <unstatu@gmail.com>
2015-09-28 00:50:02 +0200

    Typo

--- scripts/install_drupal7.sh
+++ scripts/install_drupal7.sh
@@ -5,4 +5,4 @@ echo $PROJECTNAME;
 
 docker exec -i --user=1000:1000 $PROJECTNAME-apache composer global require drush/drush:7.*
 docker exec $PROJECTNAME-apache ln -s /home/www-bridge-user/.composer/vendor/bin/drush /usr/bin/drush
-docker exec -i --user=1000:1000 $PROJECTNAME-apache sh -c 'º	drush dl -y --destination=/var/www/html --drupal-project-rename=drupal7 drupal && cd /var/www/html/drupal7 && drush si --db-url=mysql://root:123@db/drupal7 -y'
+docker exec -i --user=1000:1000 $PROJECTNAME-apache sh -c 'drush dl -y --destination=/var/www/html --drupal-project-rename=drupal7 drupal && cd /var/www/html/drupal7 && drush si --db-url=mysql://root:123@db/drupal7 -y'
