56710e2560910be25db3b2d769a11f9520a7a4af
Carlos Reig Matut <unstatu@gmail.com>
2015-12-05 16:23:28 +0100

    Removed the old bash files

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-FILEPATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)/`basename "${BASH_SOURCE[0]}"`
-SCRIPTDIR=`dirname $FILEPATH`
-PROJECTNAME=`basename $SCRIPTDIR` | tr '[:upper:]' '[:lower:]'
-PROJECTNAME=`basename $SCRIPTDIR`
-
-docker start $PROJECTNAME-mysql $PROJECTNAME-mail $PROJECTNAME-apache;
