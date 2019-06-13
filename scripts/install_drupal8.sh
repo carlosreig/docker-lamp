310550d82013367448c2095aa1a5bbf845d09dfd
Carlos Reig Matut <unstatu@gmail.com>
2019-06-13 11:31:39 +0200

    Fixed d8 instalation script

--- scripts/install_drupal8.sh
+++ scripts/install_drupal8.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-docker-compose exec --user=1000:1000 web composer global require drush/drush:8.x-dev
-docker-compose exec web ln -s /home/www-bridge-user/.composer/vendor/bin/drush /usr/bin/drush
-docker-compose exec --user=1000:1000 web sh -c 'drush dl -y --destination=/var/www/html --drupal-project-rename=drupal8 drupal && cd /var/www/html/drupal8 && drush si --db-url=mysql://root:123@db/drupal8 -y'
+cd "$parent_path"
+
+docker-compose exec web sh -c "sed -i -e 's/www\/html/www\/html\/web/g' /etc/apache2/sites-enabled/000-default.conf"
+docker-compose exec web sh -c "sed -i -e 's/www\/html/www\/html\/web/g' /etc/apache2/sites-enabled/default-ssl.conf"
+#rm -rf ../www;
+#cd .. && composer create-project drupal-composer/drupal-project:8.x-dev www
+../stop.sh
+../start.sh
+docker-compose exec --user=1000:1000 web sh -c 'vendor/bin/drush si standard --db-url=mysql://root:123@db/drupal8 -y'
